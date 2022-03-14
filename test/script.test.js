4b7e7d45c49014fdd71ca77e203312b075a079bb
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-03-15 12:32:44 +1300

    added test for path to slime, as i expected the test passed with out any changes to scripts.js

--- test/script.test.js
+++ test/script.test.js
@@ -25,22 +25,16 @@ describe("getBotReply", () => {
     const expectedReply1 = 
     "Hi there quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
 
-    // Uncomment the following line and update your expectation
-    expect(botReply1).toEqual(expectedReply1);
-
     const botReply2 = getBotReply("yes");
     const expectedReply2 = 
     "Mmm yes, clearly you are very cultured quin. Have you seen Death note?"
-
-    // Uncomment the following line and update your expectation
-    expect(botReply2).toEqual(expectedReply2);
 
     const botReply3 = getBotReply("no");
     const expectedReply3 = 
     "I would highly recommend Death note. An extremely worth while watch, the protagonist light yagami finds a mysterious note pad called the Death note while at school one day."
     
-
-    // Uncomment the following line and update your expectation
+    expect(botReply1).toEqual(expectedReply1);
+    expect(botReply2).toEqual(expectedReply2);
     expect(botReply3).toEqual(expectedReply3);
   });
   it("should reply correctly when follwing path to code geass", () => {
@@ -48,29 +42,21 @@ describe("getBotReply", () => {
     const expectedReply1 = 
     "Hi there quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
 
-    // Uncomment the following line and update your expectation
-    expect(botReply1).toEqual(expectedReply1);
-
     const botReply2 = getBotReply("yes");
     const expectedReply2 = 
     "Mmm yes, clearly you are very cultured quin. Have you seen Death note?"
-
-
-    // Uncomment the following line and update your expectation
-    expect(botReply2).toEqual(expectedReply2);
 
     const botReply3 = getBotReply("yes");
     const expectedReply3 = 
     "Fantastic, I enjoyed Death note, did you enjoy death-note as well?"
     
-    // Uncomment the following line and update your expectation
-    expect(botReply3).toEqual(expectedReply3);
-
     const botReply4 = getBotReply("yes");
     const expectedReply4 = 
     "Excellent, I suggest you watch Code Geass. The main character Lelouch vi Britannia battles against the odds as he fights to regain connote of his home country."
     
-    // Uncomment the following line and update your expectation
+    expect(botReply1).toEqual(expectedReply1);
+    expect(botReply2).toEqual(expectedReply2);
+    expect(botReply3).toEqual(expectedReply3);
     expect(botReply4).toEqual(expectedReply4);
     
   });
@@ -79,23 +65,40 @@ describe("getBotReply", () => {
     const expectedReply1 = 
     "Hi there quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
 
-
     const botReply2 = getBotReply("no");
     const expectedReply2 = 
     "How Exciting! You are at the beginning of your journey and there are so many options! Does an R16 rating concern you?"
-
-
-    
 
     const botReply3 = getBotReply("no");
     const expectedReply3 = 
     "No? fantastic! I recommend the masterpiece Attack on Titan, main character Eren Yeager has lived inside the walls his entire life, one fateful day everything changed."
     
-  
     expect(botReply1).toEqual(expectedReply1);
     expect(botReply2).toEqual(expectedReply2);
     expect(botReply3).toEqual(expectedReply3);
     
   });
+  it("should reply correctly when follwing path to slime", () => {
+    const botReply1 = getBotReply("quin");
+    const expectedReply1 = 
+    "Hi there quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
+
+    const botReply2 = getBotReply("yes");
+    const expectedReply2 = 
+    "Mmm yes, clearly you are very cultured quin. Have you seen Death note?"
+
+    const botReply3 = getBotReply("yes");
+    const expectedReply3 = 
+    "Fantastic, I enjoyed Death note, did you enjoy death-note as well?"
+    
+    const botReply4 = getBotReply("no");
+    const expectedReply4 = 
+    "Poor Light Yagami, seeing as you didn't enjoy death note so much, I'm going to suggest something that sets a completely different pace, that time I was reincarnated as a slime"
+    
+    expect(botReply1).toEqual(expectedReply1);
+    expect(botReply2).toEqual(expectedReply2);
+    expect(botReply3).toEqual(expectedReply3);
+    expect(botReply4).toEqual(expectedReply4);
+});
 });
 
