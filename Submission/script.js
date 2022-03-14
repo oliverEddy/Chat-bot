4bd37031c0898dc5e46ecddc35560a10cb854f0d
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-03-14 18:02:43 +1300

    wrote first functions in scripts.js, the function passes the test i wrote in script.test.js, the function resets to level one if user inputs reset, if level if 1 it displays the opening message asking for the users name, the value of user name is assigned to let userName if userName is undefinded, the function returns stating the users name and asking the first yes/ no question, funcition has a path for the yes response

--- Submission/script.js
+++ Submission/script.js
@@ -15,22 +15,28 @@ const noReplys = ["no", "n", "nah"];
 
 
 
+
 const getBotReply = (msg) => {
   const userInput = msg.toLowerCase();
   if (userInput === "reset") {
     level = 1 
     return`Hi there ${userName}, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?`;
-};
+}
 
   if (level === 1){
     if (userName === undefined)
     userName = userInput
-    level = 1
+    level = 2
     return`Hi there ${userName}, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?`;
   }  
-  
- 
+  if (yesReplys.includes(userInput)){
+    level = 2
+    path = 1;
+    return `Mmm yes, clearly you are very cultured ${userName}. Have you seen Death note?`;
   }
+  };
+ 
+  
 
   export { getBotReply };
  
--- test/script.test.js
+++ test/script.test.js
@@ -14,7 +14,7 @@ describe("getBotReply", () => {
   it("should reply with Name and first question", () => {
     const botReply1 = getBotReply("Quin");
     const expectedReply1 = 
-    "Hi there Quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
+    "Hi there quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
 
     // Uncomment the following line and update your expectation
     expect(botReply1).toEqual(expectedReply1);
@@ -23,13 +23,13 @@ describe("getBotReply", () => {
   it("should reply correctly when supplised YES answer to, Have you watched anime beofre", () => {
     const botReply1 = getBotReply("Quin");
     const expectedReply1 = 
-    "Hi there Quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
+    "Hi there quin, so you need help deciding what anime to watch. Lucky for you that's my speciality! Now have you watched any anime before?";
 
     // Uncomment the following line and update your expectation
     expect(botReply1).toEqual(expectedReply1);
-    const botReply2 = getBotReply("Yes");
+    const botReply2 = getBotReply("yes");
     const expectedReply2 = 
-    "Mmm yes, clearly you are very cultured Quin. Have you seen Death note?"
+    "Mmm yes, clearly you are very cultured quin. Have you seen Death note?"
 
     // Uncomment the following line and update your expectation
     expect(botReply2).toEqual(expectedReply2);
